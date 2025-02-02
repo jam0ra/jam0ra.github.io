92f29eb77858a950b46b7c112a69000ce6a4ace8
jjamora <jjamora@sfu.ca>
2025-02-01 22:07:14 -0800

    Fixed next.config.ts settings to work with GitHub pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',  // Enables static HTML export
+  basePath: '/jam0ra.github.io', // Your repository name
+  images: {
+    unoptimized: true,
+  },
+  // Disable server-side features since GitHub Pages is static
+  trailingSlash: true,
 };
 
 export default nextConfig;
